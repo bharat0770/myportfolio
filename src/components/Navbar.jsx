786a52a847a2c6acca4d312aa6777397c088a357
Bharat Suthar <154978824+bharat0770@users.noreply.github.com>
2026-01-02 10:28:36 +0530

    resume updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,11 @@ function Navbar() {
           <a href="#footer" className='relative hover:text-blue-800 group'>
             contact
             <div className='absolute left-0 -bottom-1 h-1 z-10 bg-blue-800  w-0 transition-all duration-150 ease-out group-hover:w-full'></div>
-          </a>  <a href="https://drive.google.com/file/d/1dE4gM5Jja5B2SvGjyMk-dGi6fxZlX6cD/view?usp=sharing" target='_blank' className='relative hover:text-blue-800 group'>
+          </a>
+          {/* <a href="https://drive.google.com/file/d/1dE4gM5Jja5B2SvGjyMk-dGi6fxZlX6cD/view?usp=sharing" target='_blank' className='relative hover:text-blue-800 group'> */}
+
+          <a href="https://drive.google.com/file/d/1VwSqB5fjBB5f1G0wX9eNNudwvKjbU75E/view?usp=sharing" target='_blank' className='relative hover:text-blue-800 group'>
+
             resume
             <div className='absolute left-0 -bottom-1 h-1 z-10 bg-blue-800  w-0 transition-all duration-150 ease-out group-hover:w-full'></div>
           </a>
